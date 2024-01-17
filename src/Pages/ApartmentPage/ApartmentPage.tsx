2254bde06101fd87a8c933a1e73087dc397db0d1
Shani Yaish <yaish_shani@walla.com>
2024-01-17 16:01:14 +0200

    changes

---
+++ src/Pages/ApartmentPage/ApartmentPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const ApartmentPage: React.FC = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to My React App</h1>
+      <p>This is the home page content.</p>
+    </div>
+  );
+};
+
+export default ApartmentPage;
