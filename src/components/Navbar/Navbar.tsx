6e96ff2d476b0b7ab7173304f9cfc84ebbe94dfb
Shiraz <shirazsorijoun@gmail.com>
2024-01-21 22:23:34 +0200

    Navbar update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
+/*
 import React from 'react';
 import Button from '@mui/material/Button';
 import './Navbar.css';
+
 
 const Navbar: React.FC = () => {
   return (
@@ -18,3 +20,50 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
+*/
+
+
+
+import React, { useState } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div className="nav">
+      <div className="logo">
+        <img
+          className="logo-image"
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/558c225b22f6ca9794d5a1e7ed80500bb2f1567d453a4a0817759fd3263cff87?apiKey=0a183be08c194c808182f759b68acaaf&"
+          alt="Logo"
+        />
+        Fix<span style={{ color: "rgba(242,153,74,1)" }}>N</span>Drive.
+      </div>
+      <div className="pages">
+        <div className="page">Home</div>
+        <div className="page">Rent</div>
+        <div className="page">Profile</div>
+        <div className="page">Contact Us</div>
+      </div>
+      <div className="auth-buttons">
+        {isLoggedIn ? (
+          <div className="auth-button" onClick={() => setIsLoggedIn(false)}>
+            Logout
+          </div>
+        ) : (
+          <>
+            <div className="auth-button">Login</div>
+            <div className="auth-button">Sign up</div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+
+
+
