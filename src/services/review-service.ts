f09fab5970e2136c46232495238f3d71c648320b
Shani Yaish <yaish_shani@walla.com>
2024-01-30 18:30:05 +0200

    addReview component

---
+++ src/services/review-service.ts
@@ -0,0 +1,25 @@
+import apiClient, { CanceledError } from "./api-client"
+import { ReviewProps } from "../types/types";
+
+export { CanceledError }
+
+const getAllReviews = () => {
+    const abortController = new AbortController()
+    const req = apiClient.get<ReviewProps[]>('userReview', { signal: abortController.signal })
+    return { req, abort: () => abortController.abort() }
+}
+
+const postReview = (review: ReviewProps, token: string) => {
+    const abortController = new AbortController();
+    const req = apiClient.post("userReview/create", {
+        review: review,
+    }, {
+      signal: abortController.signal,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return { req, abort: () => abortController.abort() };
+  };
+
+export default { getAllReviews,postReview };
