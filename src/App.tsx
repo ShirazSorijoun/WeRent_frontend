f1c76ed0e3332367df818ff59f8ca7ac78b996a1
Shani Yaish <yaish_shani@walla.com>
2024-01-30 18:34:36 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import AddApartment from './components/AddApartment/addApartment';
+//import AddApartment from './components/AddApartment/addApartment';
 //import Apartment from './components/Apartments/apartments';
+//import Apartment from './components/Apartments/apartmentDetails.tsx';
+//import addReview from"./components/AddReview/addReview"
+import Reviews from"./components/Reviews/reviews"
+
+
+
 import './App.css';
 
 const App: React.FC = () => {
+
+  //const apartmentId = '65b8c6fd1f15e9e5872ac051';
+
   return (
     <div className="App">
-      <AddApartment />
+      <Reviews />
     </div>
   );
 }
