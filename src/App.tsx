1c796ba5130885370607154618ec46bb01cfd6e6
Shiraz <shirazsorijoun@gmail.com>
2024-02-07 22:43:03 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import Login from "./components/Login/login";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import Navbar from "./components/Navbar/Navbar";
 import SignUpPage from "./components/Registration/Registration";
-//import UserPage from "./pages/UserPage/UserPage";
-//import AddApartmentPage from "./pages/AddApartmentPage/AddApartmentPage";
+import UserPage from "./pages/UserPage/userPage";
+import AddApartmentPage from "./pages/AddApartmentPage/addApartmentPage";
 
 const App: React.FC = () => {
   //const apartmentId = '65c254369bfa1933808ec5e1';
@@ -23,8 +23,8 @@ const App: React.FC = () => {
           <Route path="/reviews" element={<Reviews />} />
           <Route path="/login" element={<Login />} />
           <Route path="signup" element={<SignUpPage/>} />
-          {/*<Route path="/profile" element={<UserPage/>} />*/}
-          {/*<Route path="/addApartment" element={AddApartmentPage} /> */} 
+          <Route path="/profile" element={<UserPage/>} />
+          <Route path="/addApartment" element={<AddApartmentPage/>} />
 
         </Routes>
       </div>
