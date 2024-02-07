f51e709ef9a4ad2ad0a2304bde00bc4fe8ff559b
Shani Yaish <yaish_shani@walla.com>
2024-02-07 19:55:50 +0200

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from "react";
 import "./App.css";
 
-//import AddApartment from "./components/AddApartment/addApartment";
-//import ApartmentDetails from "./components/ApartmentDetails/apartmentDetails";
-//import AddReview from "./components/AddReview/addReview";
-//import Login from "./components/Login/login"
-import UserProfile from "./components/UserProfile/userProfile"
-import { BrowserRouter as Router } from "react-router-dom";
-//import Registration from "./components/Registration/Registration";
-
+import UserPage from "./pages/UserPage/userPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import AddApartmentPage from "./pages/AddApartmentPage/addApartmentPage";
 
 const App: React.FC = () => {
-  //const apartmentId = '65c254369bfa1933808ec5e1';
-
   return (
     <Router>
-      <div className="App">
-        <UserProfile/>
-      </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/profile" element={<UserPage />} />
+        <Route path="/addapartment" element={<AddApartmentPage />} />
+      </Routes>
     </Router>
   );
 };
-
-export default App;
