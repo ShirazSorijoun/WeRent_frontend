eaeb893ce6b9d7546efe06a0c29f07e7e4352d04
Shiraz <shirazsorijoun@gmail.com>
2024-02-07 16:47:31 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import "./App.css";
 
-import AddApartment from "./components/AddApartment/addApartment";
-import ApartmentDetails from "./components/ApartmentDetails/apartmentDetails";
-import AddReview from "./components/AddReview/addReview";
-import Login from "./components/Login/login"
-//import { BrowserRouter as Router } from "react-router-dom";
-import Registration from "./components/Registration/Registration";
-import { BrowserRouter as Router } from "react-router-dom";
+//import AddApartment from "./components/AddApartment/addApartment";
+//import ApartmentDetails from "./components/ApartmentDetails/apartmentDetails";
+//import AddReview from "./components/AddReview/addReview";
+//import Login from "./components/Login/login"
+//import Registration from "./components/Registration/Registration";
+import { BrowserRouter as Router} from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Footer from "./components/Footer/Footer";
 
 const App: React.FC = () => {
-  const apartmentId = '65c254369bfa1933808ec5e1';
+  //const apartmentId = '65c254369bfa1933808ec5e1';
 
   return (
     <Router>
       <div className="App">
-        <Registration />
+        <LandingPage />
+        <Footer />
       </div>
     </Router>
   );
