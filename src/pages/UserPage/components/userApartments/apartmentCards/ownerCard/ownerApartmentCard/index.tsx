7be3a8dce2ecd7731abc6eb55bf32c04e919d66d
galronen10 <galronen10@gmail.com>
2024-08-30 14:20:11 +0300

    add move to private area to apartment that the user own

--- src/pages/UserPage/components/userApartments/apartmentCards/ownerCard/ownerApartmentCard/index.tsx
+++ src/pages/UserPage/components/userApartments/apartmentCards/ownerCard/ownerApartmentCard/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { IApartment } from '@/models/apartment.model';
 
 import { OwnerApartmentMatches } from '../ownerApartmentMatches';
 import { IMatch } from '@/models/match.model';
 import { BasicUserApartmentCard } from '../../basicUserApartmentCard';
+import { SendToApartmentPersonalArea } from '@@/sendToApartmentPersonalArea';
 
 interface IUserApartmentCardProps {
   apartment: IApartment;
@@ -14,10 +15,17 @@ export const OwnerApartmentCard: React.FC<IUserApartmentCardProps> = ({
   apartment,
   matchesList,
 }) => {
+  const cardActions = useMemo(
+    () =>
+      apartment.leaseId ? (
+        <SendToApartmentPersonalArea apartmentId={apartment._id} />
+      ) : (
+        <OwnerApartmentMatches matchesList={matchesList} />
+      ),
+    [apartment._id, apartment.leaseId, matchesList],
+  );
+
   return (
-    <BasicUserApartmentCard
-      apartment={apartment}
-      cardActions={<OwnerApartmentMatches matchesList={matchesList} />}
-    />
+    <BasicUserApartmentCard apartment={apartment} cardActions={cardActions} />
   );
 };
